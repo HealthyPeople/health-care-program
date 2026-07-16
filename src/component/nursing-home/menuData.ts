1405be0887dd38d444a400b947b7aee653a1cb38
yeowool1010 <qyoong3579@gmail.com>
2026-07-17 01:18:13 +0900

    ✨ [Feat] 프로그램 의견수렴 및 반영 페이지 구현

--- src/component/nursing-home/menuData.ts
+++ src/component/nursing-home/menuData.ts
@@ -132,8 +132,8 @@ export const sections3: Sections3 = {
     '월 프로그램 수행계획': [
       { name: '월 프로그램 수행계획 등록', link: '/nursingHome/monthly-program-plan' },
     ],
-    '프로그램 의견수렴 및 반영(신규/임의개발)': [
-      { name: '프로그램 의견수렴 및 반영(신규/임의개발)', link: '/nursingHome/program-feedback' },
+    '프로그램 의견수렴 및 반영(신규페이지)': [
+      { name: '프로그램 의견수렴 및 반영(신규페이지)', link: '/nursingHome/program-feedback' },
     ],
   },
   '요구도관리': {
